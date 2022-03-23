9456f96ec903f50fbd5406492138cce253d6d3f5
Baskara Sethupathi S <bask3022@global.chainsys.com>
2022-03-23 15:55:49 +0530

    include commit

--- challenges/JS challenges/advice-generator-app/script/advise-script.js
+++ challenges/JS challenges/advice-generator-app/script/advise-script.js
@@ -1,15 +1,19 @@
-const id = document.getElementById("id");
-const advice = document.getElementById("advice");
-async function getData(url) {
-  const response = await fetch(url);
-  var data = await response.json();
-  showData(data);
+const id = document.getElementById('id');
+const advice = document.getElementById('advice');
+const url = 	"https://api.adviceslip.com/advice";
+
+async function getData(url){
+    const response = await fetch(url);
+    var data = await response.json();
+    showData(data);
 }
 getData(url);
-function showData(data) {
-  id.innerHTML = data.slip.id;
-  advice.innerHTML = data.slip.advice;
+function showData (data){
+    id.innerHTML = data.slip.id;
+    advice.innerHTML = data.slip.advice;
 }
-document.querySelector("button").addEventListener("click", (e) => {
-  getData(url);
-});
+
+
+document.querySelector('button').addEventListener('click',(e)=>{
+    getData(url);
+})
