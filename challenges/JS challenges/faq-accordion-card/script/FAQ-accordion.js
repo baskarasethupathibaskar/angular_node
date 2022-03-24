37c307a5a682bd8f4fbe018f5eec9037b7d6a9c8
Baskara Sethupathi S <bask3022@global.chainsys.com>
2022-03-24 14:30:54 +0530

    include commit

--- challenges/JS challenges/faq-accordion-card/script/FAQ-accordion.js
+++ challenges/JS challenges/faq-accordion-card/script/FAQ-accordion.js
@@ -1,14 +1,32 @@
-this.addEventListener("DOMContentLoaded", () =>{
-    const questions = document.querySelectorAll(".question")
-    questions.forEach((question) => question.addEventListener("click", () =>{
-  
-        if(question.parentNode.classList.contains("active")){
-            question.parentNode.classList.toggle("active")
-        }
-    else{
-    questions.forEach(question => question.parentNode.classList.remove("active"))
-    question.parentNode.classList.add("active")
-        }
+const questionBtn = document.querySelectorAll(".question-btn");
+const answer = document.querySelectorAll(".answer");
+const questionBox = document.querySelectorAll(".question-box");
+const arrows = document.querySelectorAll(".arrow");
 
-    }))
-}) 
+const showAnswer = (e) => {
+  if (!e.target.parentNode.classList.contains("active")) {
+    questionBox.forEach((questionBox) =>
+      questionBox.classList.remove("active")
+    );
+    questionBtn.forEach((btn) => (btn.style.fontWeight = "400"));
+    arrows.forEach((arrow) => (arrow.style.transform = ""));
+  }
+
+  if (e.target.style.fontWeight == "bold") {
+    e.target.style.fontWeight = "";
+  } else {
+    e.target.style.fontWeight = "bold";
+  }
+
+  if (e.target.children[0].style.transform == "") {
+    e.target.children[0].style.transform = "rotate(180deg)";
+  } else {
+    e.target.children[0].style.transform = "";
+  }
+
+  e.target.parentNode.classList.toggle("active");
+  // e.target.parentNode.children[1].classList.toggle('active')
+  console.log(e.target.children[0]);
+};
+
+questionBtn.forEach((btn) => btn.addEventListener("click", showAnswer));
